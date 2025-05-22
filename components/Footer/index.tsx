44ce9382b2bf0742c1a790e2d5f2689bf9a9b48a
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2025-05-22 13:59:04 +0600

    update: cp

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -200,9 +200,9 @@ const Footer = () => {
           <div className="h-px w-full bg-gradient-to-r from-transparent via-[#D2D8E183] to-transparent dark:via-[#959CB183]"></div>
           <div className="py-8">
             <p className="text-center text-base text-body-color dark:text-white">
-              Template by TaShanto and{" "}
-              <a href="https://nextjstemplates.com" rel="nofollow noopener">
-                Next.js Templates
+              Template by 
+              <a href="https://mdtanvirahamedshanto.vercel.app" rel="nofollow noopener">
+                Shanto
               </a>
             </p>
           </div>
